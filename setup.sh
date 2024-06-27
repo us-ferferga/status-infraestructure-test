afade136ac2d59807a943546d1ee5436b9810765
Fernando Fernández <ferferga@hotmail.com>
2024-06-27 15:16:51 +0000

    fix: wrong permissions in node-red container
    If a folder is not created before doing a bind mount in Docker,
    the folder will be created with root permissions only.
    
    Signed-off-by: GitHub <noreply@github.com>

--- setup.sh
+++ setup.sh
@@ -8,6 +8,9 @@ cp .env.deploy ./status-backend/.env
 
 cp .env.deploy .env
 
+## If a folder is not created before doing a bind mount in Docker, the folder will be created with root permissions only.
+mkdir -p node-red-status
+
 docker compose -p status up -d 
 
 echo "Setup completado. Accede a http://localhost:3000 para ver la aplicación o a http://localhost:3001/docs para acceder a la documentación de la API."
